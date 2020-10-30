715660cf7d5f182f0d71d56d69df293d336474df
wild-deer <7373281+jeremy_ne@user.noreply.gitee.com>
2020-10-30 12:15:40 +0800

    新增红绿灯、车辆数据传输

--- 2.0version.py
+++ 2.0version.py
@@ -2,15 +2,14 @@ import sys
 import cv2
 import time
 from PyQt5 import QtCore
-from PyQt5.QtCore import QThread   #引入线程
+from PyQt5.QtCore import QThread  # 引入线程
 from PyQt5.QtGui import QImage, QPixmap, QColor
 
-from Algorithm.toyDetect import NLToyDetect #引入算法sdk类
+from Algorithm.toyDetect import NLToyDetect  # 引入算法sdk类
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from TL_UI import Ui_MainWindow
+import tct_trans
 
-from LightTime import LightTime
-from UI.UI import Ui_MainWindow
-#from requsts import requst
 
 # 增加对非汽车的过滤
 
@@ -21,7 +20,6 @@ class CameraThread(QThread):  # 摄像头线程类
     def __init__(self, mw):
         self.mw = mw  # UI界面的对象
         self.working = True
-        self.car_count = 0
         QThread.__init__(self)
 
     def __del__(self):
@@ -35,6 +33,7 @@ class CameraThread(QThread):  # 摄像头线程类
                 self.mw.CapIsbasy = True
                 # 调用UI界面的摄像头对象，获取图片
                 ret, image = self.mw.cap.read()  # 获取新的一帧图片
+                time.sleep(1/30)
                 if not ret:  # 如果ret不为0意味着获取图片失败
                     print("Capture Image Failed")
                     self.mw.CapIsbasy = False
@@ -46,6 +45,7 @@ class CameraThread(QThread):  # 摄像头线程类
                     self.mw.limg = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)  # 颜色空间转换
                 self.mw.CapIsbasy = False
                 self.updatedM.emit(self.mw.frameID)  # 触发当前事件附加参数都会传给监听器回调
+
             else:
                 time.sleep(1.0 / 50)  # 间隔时间 再开始获取图片
 
@@ -54,10 +54,8 @@ class CameraThread(QThread):  # 摄像头线程类
             self.working = False
             print('摄像头采集线程退出')
 
-
-class ToyDetectThread(QThread):
-    # 物品目标检测算法sdk调用线程
-    updatedImage = QtCore.pyqtSignal(int)  # 定义一个信号
+class LedThread(QThread):#红绿灯线程类
+    updatedcar = QtCore.pyqtSignal(int)  # 定义一个信号
 
     def __init__(self, mw):
         self.mw = mw  # 传入UI界面对象
@@ -67,12 +65,53 @@ class ToyDetectThread(QThread):
     def __del__(self):
         self.wait()
 
+    def run(self):
+        while self.working:
+            pass
+            # print(str(self.mw.car1))
+    def stop(self):
+        if self.working:
+            self.working = False
+            print('ToyDetectThread线程退出了')
+
+
+class tcp_trans(QThread):  # 红绿灯线程类
+    updatedcar = QtCore.pyqtSignal(int)  # 定义一个信号
+
+    def __init__(self, mw):
+        self.mw = mw  # 传入UI界面对象
+        self.working = True
+        QThread.__init__(self)
+
+
+    def __del__(self):
+        self.wait()
 
     def run(self):
-        
-        #初始化车辆数
-        self.cur_time = time.time()
-        self.lt = LightTime(self)
+        while self.working:
+            tct_trans.send_carNumber(self.mw.tcp_client,self.mw.car1)
+            time.sleep(1)
+            print(str(self.mw.car1))
+
+    def stop(self):
+        if self.working:
+            self.working = False
+            print('ToyDetectThread线程退出了')
+
+
+class ToyDetectThread(QThread):
+    # 物品目标检测算法sdk调用线程
+    updatedImage = QtCore.pyqtSignal(int)  # 定义一个信号
+    updatedcar = QtCore.pyqtSignal(int)  # 定义一个信号
+    def __init__(self, mw):
+        self.mw = mw  # 传入UI界面对象
+        self.working = True
+        QThread.__init__(self)
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
         while self.working:
             if self.mw.AlgIsbasy == False and not (self.mw.limg is None):  # 表示成功获取图片
                 self.mw.AlgIsbasy = True  # 开始处理图片
@@ -90,12 +129,13 @@ class ToyDetectThread(QThread):
 
                             # outObject是 Algorithm.toyDetect.Struct_TD_ObjInfor 类型变量
 
+                            # print(type(outObject))
+                            # print(outObject.className == "car")
 
-                            print(outObject.className == "car")
+
                             if str(outObject.className, "utf-8").replace('\r', '') == "car":
-                                #对车的数量进行计数
-                                self.car_count +=1
-
+                                self.mw.car_number_1.setText(str(ret))
+                                self.mw.car1 = ret
                                 font = cv2.FONT_HERSHEY_SIMPLEX  # 定义字体
                                 # 在图片上，标识类别名称
                                 imgzi = cv2.putText(rgb, str(outObject.className, "utf-8").replace('\r', ''),
@@ -124,8 +164,9 @@ class ToyDetectThread(QThread):
                                 # 画出物体框
                                 cv2.rectangle(rgb, (int(outObject.dwLeft), int(outObject.dwTop)),
                                               (int(outObject.dwRight), int(outObject.dwBottom)), (0, 0, 255), 2)
-                                print('类别名：' + str(outObject.className, "utf-8").replace('\r', '') + '  置信度: ' + str(
-                                    outObject.fscore))  # 打印结果在命令行
+                                # print('类别名：' + str(outObject.className, "utf-8").replace('\r', '') + '  置信度: ' + str(
+                                #     outObject.fscore))  # 打印结果在命令行
+
                                 """
                                     rectangle(img, pt1, pt2, color[, thickness[, lineType[, shift]]]) -> img
                                     .   @brief Draws a simple, thick, or filled up-right rectangle.
@@ -154,22 +195,11 @@ class ToyDetectThread(QThread):
                     showImage = QImage(rgb.data, width, height, bytesPerLine, QImage.Format_RGB888)  # 将处理过的图片保存用于显示
                     self.mw.showImage = QPixmap.fromImage(showImage)
 
-                    if self.cur_time + 10 <= time.time():  # 每隔10s统计车辆数
-
-                        self.lt.set_total_car(self.car_count, 1)  # 返回车辆数 #(x,y):车辆数
-                        print("SECOND", self.lt.cur_seconds_x_red, self.lt.cur_seconds_x_green)
-
-                        self.mw.R_time = self.lt.cur_seconds_x_red
-                        self.mw.G_time = self.lt.cur_seconds_x_green
-                        # self.Updata = requst(self)
-                        # self.Updata.updata(self.car_count)         #上传车辆数
-                        print("车辆数：",self.car_count)
-                        self.car_count = 0
-                        self.cur_time = time.time()  # 重置当前时间
                     self.updatedImage.emit(self.mw.frameID)  # 触发信号，去执行UI界面的相关函数
+                    self.updatedcar.emit(ret)
                 else:
                     print('Error code1:', ret)
-                    time.sleep(0.0001)
+                    time.sleep(100)
                 self.mw.AlgIsbasy = False
             else:
                 time.sleep(0.001)
@@ -181,39 +211,40 @@ class ToyDetectThread(QThread):
 
 
 
-'''class LedThread(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super(LedThread, self).__init__()
-        self.setupUi(self)
-        self.AlgIsbasy = False
-        self.CapIsbasy = False
-        self.frameID = 0
-'''
+
 
 class TL_Page(QMainWindow, Ui_MainWindow):  # 窗口页面
     def __init__(self):  # 初始化界面函数
         super(TL_Page, self).__init__()  # 继承UI
         self.setupUi(self)
-        #黄灯
-        self.yellow_time.setText("3s")
-        self.yellow_time.setStyleSheet("font: 18pt \"Abaddon™\";")
-        self.red_time.setText("10s")
-        self.red_time.setStyleSheet("font: 18pt \"Abaddon™\";")
-        self.green_time.setText("10s")
-        self.green_time.setStyleSheet("font: 18pt \"Abaddon™\";")
-        self.R_time = 10
-        self.G_time = 10
+
         # 设置线程参数,在线程中会用到所以提前 定义
         self.AlgIsbasy = False
         self.CapIsbasy = False
         self.frameID = 0
+        self.car1 = 0  #路口一的车辆数保存在这里
+        self.car2 = 0
+
+        # 初始红绿灯参数
+        # self.red_time.setText(str(QColor('red')))
+        # self.red_time.setStyleSheet("background-color: red")
+        # self.yellow_time.setText("     2s")
+        # self.green_time.setText("     10s")
+
+        self.car_number_1.setText("0")
+
 
         # 设置摄像头视频采集参数
-        self.cap = cv2.VideoCapture(0)  # 0是笔记本自带摄像头，可参数：“视频路径”
+        self.cap = cv2.VideoCapture("2.avi")  # 0是笔记本自带摄像头，可参数：“视频路径”
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)  # 设置分辨率
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
         self.limg = None  # 将摄像头线程采集的一帧图片保存在这里
         self.showImage = None  # 将经过处理的图片保存在这里
+
+
+        # tcp传输类
+        self.tcp_client = tct_trans.init_tcp()
+
 
         # 调用算法类-------实例化算法sdk类，并初始化加载模型和配值
         self.configPath = configPath  # 模型防止路径信息
@@ -225,23 +256,12 @@ class TL_Page(QMainWindow, Ui_MainWindow):  # 窗口页面
 
         self.start_btn.clicked.connect(self.start_btn_func)  # 开始按钮，通过点击触发函数
         self.stop_btn.clicked.connect(self.stop_btn_func)  # 停止按钮，通过点击触发函数
-
+    def setCarnumber(self):
+        self.car_number_1.setText(str(self.car1))
     def showframe(self):
         # 显示视频流
         self.label_show_camera.setPixmap(self.showImage)
-        self.red_time.setText(str(self.R_time) + 's')
-        self.red_time.setStyleSheet("font: 18pt \"Abaddon™\";")
 
-        self.green_time.setText(str(self.G_time) + 's')
-        self.green_time.setStyleSheet("font: 18pt \"Abaddon™\";")
-
-    def Display_time(self):
-        #显示红绿灯时间
-        self.red_time.setText(str(self.R_time)+'s')
-        self.red_time.setStyleSheet("font: 18pt \"Abaddon™\";")
-
-        self.green_time.setText(str(self.G_time)+'s')
-        self.green_time.setStyleSheet("font: 18pt \"Abaddon™\";")
     def start_btn_func(self):
         self.start_btn.setEnabled(False)
         self.stop_btn.setEnabled(True)
@@ -254,8 +274,14 @@ class TL_Page(QMainWindow, Ui_MainWindow):  # 窗口页面
         # 线程2算法处理
         self.toy_detect_th = ToyDetectThread(self)
         self.toy_detect_th.updatedImage.connect(self.showframe)  # 将线程信号绑定在显示函数上
+        self.toy_detect_th.updatedcar.connect(self.setCarnumber)  # 将线程信号绑定在显示函数上
         self.toy_detect_th.start()
-
+        #线程3红绿灯处理
+        self.led_th = LedThread(self)
+        self.led_th.start()
+        #tcp线程处理
+        self.tcp_trans_th = tcp_trans(self)
+        self.tcp_trans_th.start()
     def stop_btn_func(self):
         self.stop_btn.setEnabled(False)
         self.start_btn.setEnabled(True)
@@ -285,3 +311,15 @@ if __name__ == '__main__':
 
     sys.exit(app.exec_())
 
+# 调用笔记本自带摄像头
+'''cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    cv2.imshow("Video", frame)
+    # 读取内容
+    if cv2.waitKey(10) == ord("q"):
+        break
+
+# 随时准备按q退出
+cap.release()
+cv2.destroyAllWindows()'''
